16370b7f53e4be15a003cd0e65b756a1e12ab7fa
doddy.ph <d0dd33ph@gmail.com>
2015-05-22 06:48:38 +0000

    Add get elastic mapping

--- project_dj/app_dj/urls.py
+++ project_dj/app_dj/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 import views
 
 urlpatterns = [
-    url(r'^build-an-index/$', views.api_elastic_sample_build_an_index, name='api-elastic-sample-build-an-index')
+    url(r'^build-an-index/$', views.api_elastic_sample_build_an_index, name='api-elastic-sample-build-an-index'),
+    url(r'^mapping/$', views.api_elastic_mapping, name='api-elastic-mapping'),
 ]
--- project_dj/app_dj/views.py
+++ project_dj/app_dj/views.py
@@ -1,4 +1,5 @@
 import csv
+import json
 import urllib
 from django.http import JsonResponse
 from elasticsearch import Elasticsearch
@@ -49,3 +50,33 @@ def api_elastic_sample_build_an_index(request):
     es.bulk(index=INDEX_NAME, body=bulk_data, refresh=True)
 
     return JsonResponse({'status': 'Build a sample an Elastic index done.'})
+
+
+class Object:
+    def to_JSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__)
+
+
+def api_elastic_mapping(request):
+    es = Elasticsearch(hosts=[ES_HOST])
+
+    mapping_res = es.indices.get_mapping()
+    mapping = []
+
+    for key, value in mapping_res.iteritems():
+        obj = Object()
+        obj.fields = []
+        obj.index = key
+
+        for key2, value2 in value['mappings'].iteritems():
+            obj.type = key2
+
+            for key3, value3 in value2['properties'].iteritems():
+                obj2 = Object()
+                obj2.name = key3
+                obj2.type = value3['type']
+                obj.fields.append(obj2)
+
+        mapping.append(json.loads(obj.to_JSON()))
+
+    return JsonResponse(mapping, safe=False)
